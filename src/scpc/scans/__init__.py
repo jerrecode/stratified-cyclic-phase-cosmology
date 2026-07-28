2843a7312a0451db61726242c8b29943de8c8ac4
Jeremy Resch <134104915+jerrecode@users.noreply.github.com>
2026-07-28 11:44:15 +0000

    feat: export scan outcome assessment API

---
+++ src/scpc/scans/__init__.py
@@ -0,0 +1,5 @@
+"""Parameter-space scan and outcome-classification utilities."""
+
+from .outcomes import OutcomeAssessment, OutcomeClass, assess_solution
+
+__all__ = ["OutcomeAssessment", "OutcomeClass", "assess_solution"]
